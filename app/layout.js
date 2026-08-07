d1c6b7ab6ff39ce17b9fa8d9b86891efc23717c3
Vaelkinkk <vaelkinh@gmail.com>
2026-08-07 18:41:43 +0700

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "AM Activator",
+  description: "AM activation dashboard"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="id">
+      <body>{children}</body>
+    </html>
+  );
+}
